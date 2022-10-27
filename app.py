e178db3fbce03b027de02ff9f682cfe706d1b5ae
JRamm91 <jordan.rammelsberg@gmail.com>
2022-10-26 21:43:58 -0500

    getting started with file organization and the beginnings of a functionaing Flask app

---
+++ app.py
@@ -0,0 +1,56 @@
+from crypt import methods
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session, jsonify
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import login_required
+
+# Configur Flask applictaion
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATE_AUTO_RELOAD"] = True
+
+# establish any jinja filters here if needed
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///diy-app.db")
+
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/")
+@login_required
+def index():
+    """Displays main page"""
+    # todo
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    if request.method == "GET":
+        return render_template("login.html")
+    # TODO make a POST
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    # todo
+    
